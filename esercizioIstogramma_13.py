4d7f01a536b343f7232110b3b57ea163e7a5e26e
massimovaresio <varesio.massimo@gmail.com>
2024-06-11 08:30:35 +0200

    Fine esercitazione 13

--- esercizioIstogramma_13.py
+++ esercizioIstogramma_13.py
@@ -18,22 +18,47 @@ def elabora_file(nomefile):
     """Riceve come argomento un file e crea un istogramma di parole
     """
     isto = dict()
-    testo_libro = open(nomefile)
+    testo_libro = open(nomefile, encoding='utf-8')  # specifico la codifica del file per evitare errori nella lettura
+    # Salta le prime 30 righe dell'intestazione
+    # '_' come variabile di iterazione, segnalo che questa variabile non sarà utilizzata nel corpo del ciclo; è una convenzione comune in Python per indicare che la variabile non ha importanza; se avessi usato anche qui 'riga' comunque funzionava
+    for _ in range(30):
+            next(testo_libro)
+        
+    # Elabora le righe rimanenti
     for riga in testo_libro:
         elabora_riga(riga, isto)
+    testo_libro.close()    
     return isto
+
 
 def elabora_riga(riga, isto):
     """Per ogni riga separa le parole, scarta spazi e punteggiatura e converti tutto il lettere minuscole
     """
-    riga.replace('-', ' ')  # metodo delle stringhe replace vedi docs https://docs.python.org/3/library/stdtypes.html#string-methods
+    riga = riga.replace('-', ' ')  # metodo delle stringhe replace vedi docs https://docs.python.org/3/library/stdtypes.html#string-methods
 
-    for parola in riga.split():
-        parola = parola.strip(string.punctuation + string.whitespace)
-        parola = parola.lower()
-        isto[parola] = isto.get(parola, 0) + 1
+    for parola in riga.split():  # suddivide la riga in una lista di stringhe
+        parola = parola.strip(string.punctuation + string.whitespace)  # rimozione della punteggiatura e degli spazi
+        parola = parola.lower()  # converti tutto in lettere minuscole (in realtà vrea una nuova stringa dato che sono immutabili)
+        isto[parola] = isto.get(parola, 0) + 1  # crea un nuovo elemento nell'istogramma o incrementa uno esistente
+
+
+def parole_totali(isto):
+     """Conta il numero di parole totali aggiungendo le frequenze dell'istogramma
+     """
+     return sum(isto.values())
+
+
+def parole_diverse(isto):
+     """Conta il numero di parole diverse contando semplicemente il numero di elementi nel dizionario
+     """
+     return len(isto)
 
 
 # ESECUZIONE
-istogramma = elabora_file('words.txt')
-print(istogramma)      
+istogramma = elabora_file('emma.txt')
+
+# Conta parole totali e stampa risultato
+print("Numero totale di parole: ", parole_totali(istogramma))
+
+# Conta parole diverse e stampa il risultato
+print("Numero di parole diverse: ", parole_diverse(istogramma))
