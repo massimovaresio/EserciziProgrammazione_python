690516b88a5e6dcf3dac39b9bbd82fb3bf1c1709
massimovaresio <varesio.massimo@gmail.com>
2024-06-30 10:14:45 +0200

    Esercizio3 lab3

---
+++ lab3_esercizio3.py
@@ -0,0 +1,11 @@
+"""Esercizio 3: Fare la somma dei numeri da 1 a 20 (1+2+....+20) usando il ciclo while
+"""
+
+num = 1
+sum = 0
+
+while num <= 20:
+    sum = sum + num
+    num = num + 1
+
+print(sum)
