90486ad6e255dcf5086be4da8bf4e399999577d2
massimovaresio <varesio.massimo@gmail.com>
2024-06-30 10:36:11 +0200

    Esecizio 4 lab3

---
+++ lab3_esercizio4.py
@@ -0,0 +1,11 @@
+"""Esercizio 4: Leggere un numero, calcolare il fattoriale e stamparlo
+"""
+
+n = int(input("Inserisci un numero "))
+fattoriale = 1
+
+for i in range(1,n+1):
+    fattoriale *= i
+    # print(fattoriale) istruzione di stampa se voglio verificare ogni ciclo
+
+print(fattoriale)
