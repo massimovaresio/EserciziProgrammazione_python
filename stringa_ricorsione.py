cd57e0f489501d08d034107c2a5f3ae4f01f5af6
massimovaresio <varesio.massimo@gmail.com>
2024-07-21 09:26:43 +0200

    Aggiornamento 21/07/2024

---
+++ stringa_ricorsione.py
@@ -0,0 +1,15 @@
+"""
+Si prega di rispettare i termini della licenza: https://creativecommons.org/licenses/by/4.0/
+"""
+
+def stampa_n_volte(s, n):
+    if n <= 0:
+        return
+    else:
+        print(s)
+        stampa_n_volte(s, n-1)
+
+stringa = input("Inserisci la stringa: ")
+numero = int(input("Inserisci il numero di volte: "))
+
+stampa_n_volte(stringa, numero)
