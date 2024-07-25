b41a1e50872feb15e8dfb808224236d41950a19a
massimovaresio <varesio.massimo@gmail.com>
2024-07-25 21:29:28 +0200

    Aggiunto file differenze print

---
+++ differenze_print.py
@@ -0,0 +1,12 @@
+"""Si prega di rispettare i termini della licenza: https://creativecommons.org/licenses/by/4.0/
+"""
+# Definiamo alcune differenze delle istruzioni di stampa con print()
+
+valore1 = 10
+valore2 = "cento"
+
+print(f"Il primo valore è {valore1}, il secondo valore è {valore2}.")
+print("")
+print("Il primo valore è", valore1, ", il secondo valore è", valore2)
+print("")
+print("Il primo valore è " + str(valore1) + ", il secondo valore è " + valore2)
